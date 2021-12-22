d6c5f3a62b29faf5d34d5d196bab9e7fc1fb23a8
Antonia Pettersson <antoniapettersson@iMac-som-tillhor-Antonia.local>
2021-12-22 10:16:24 +0100

    centrerat menu länkarna, städat bort kommeterer

--- index.js
+++ index.js
@@ -1,36 +1,6 @@
-// const toggleButton = document.getElementById("menu_bar");
-// const navbar = document.getElementById("navbar");
-
-// // const close = document.getElementById("close");
-
-// // close.addEventListener("click", () => {
-// //   navbar.classList.toggle("active");
-// // });
-
-// toggleButton.addEventListener("click", () => {
-//   console.log("click");
-//   navbar.classList.toggle("active");
-
-// });
 
 function onClickMenu(){
 	document.getElementById("menu").classList.toggle("change");
 	document.getElementById("navbar").classList.toggle("active");
 	
 }
-
-
-// const toggleButton = document.getElementById("menu_bar");
-// const navbar = document.getElementById("navbar");
-
-// // const close = document.getElementById("close");
-
-// // close.addEventListener("click", () => {
-// //   navbar.classList.toggle("active");
-// // });
-
-// toggleButton.addEventListener("click", () => {
-//   console.log("click");
-//   navbar.classList.toggle("active");
-
-// });
