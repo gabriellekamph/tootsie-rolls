9003071f4a7a834ab062408e251b5a9fa7948fb8
Petter Sjunnestrand <p.sjunnestrand@gmail.com>
2021-12-28 22:52:51 +0100

    added responsive hero section

--- index.js
+++ index.js
@@ -2,3 +2,13 @@ function onClickMenu() {
   document.getElementById("menu").classList.toggle("change");
   document.getElementById("navbar").classList.toggle("active");
 }
+
+const hero = document.querySelector(".hero");
+let heroWave = document.createElement("img");
+heroWave.src = window.innerWidth < 740 ? "./img/mobile-wave.svg" : "./img/hero-wave-desktop.svg";
+heroWave.classList.add("hero-wave");
+hero.insertAdjacentElement("beforeend", heroWave);
+
+window.addEventListener("resize", () => {
+  heroWave.src = window.innerWidth < 740 ? "./img/mobile-wave.svg" : "./img/hero-wave-desktop.svg";
+});
