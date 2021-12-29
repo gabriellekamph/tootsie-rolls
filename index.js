b4abdbdae765809d69edffb8bd9010a707706944
Gabrielle Kamph <gabriellekamph@Gabrielles-Air.lan>
2021-12-29 19:56:45 +0100

    Add function to close the cookies bar

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ function onClickMenu() {
   document.getElementById("menu").classList.toggle("change");
   document.getElementById("navbar").classList.toggle("active");
 }
+
+function closeCookies() {
+  document.getElementById("cookies").style.display = "none";
+}
